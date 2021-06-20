24c03abea3ff21ed6cb63a7c6aad03ef12f326a4
MAIDUCHUONG <maiduchuong2811@gmail.com>
2021-06-20 23:10:03 +0700

    Do an chinh thuc

--- DoAnJava/src/doanjava/DoAnJava.java
+++ DoAnJava/src/doanjava/DoAnJava.java
@@ -3,12 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.doan;
+package doanjava;
 
 /**
  *
  * @author ACER
  */
-public class Main {
+public class DoAnJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
     
 }
